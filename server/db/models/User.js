94e6086859ab2f836c545e2d8acafc18c76c8ff1
Dmitry Vasiliev <103831693+vasilievdv@users.noreply.github.com>
2022-10-25 12:48:34 +0300

    changeBirthTypeToDate

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  birthdate: { type: String, required: true },
+  birthdate: { type: Date, required: true },
   sex: { type: String, required: true },
   photo: { type: String, required: true },
 });
